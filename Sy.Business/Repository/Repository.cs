7bfe569e2f05c2342427c52599323da2dc7a1229
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-09-03 15:06:41 +0300

    repo ok

--- Sy.Business/Repository/Repository.cs
+++ Sy.Business/Repository/Repository.cs
@@ -1,42 +1,56 @@
 ﻿using Sy.Core.Abstracts;
 using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Linq.Expressions;
+using Sy.DataAccess;
 
 namespace Sy.Business.Repository
 {
     public class Repository<TEntity, TKey> : IRepository<TEntity, TKey>
-        where TEntity : IEntity<TKey>
+        where TEntity : class, IEntity<TKey>
     {
+        private readonly StockDbContext _context;
+        private readonly DbSet<TEntity> _table;
 
+        public Repository()
+        {
+            _context = new StockDbContext();
+            _table = _context.Set<TEntity>();
+        }
         public TEntity GetById(TKey id)
         {
-            throw new NotImplementedException();
+            return _table.Find(id);
         }
 
         public TKey Insert(TEntity entity)
         {
-            throw new NotImplementedException();
+            _table.Add(entity);
+            this.Save();
+            return entity.Id;
         }
 
         public int Update(TEntity entity)
         {
-            throw new NotImplementedException();
+            _table.AddOrUpdate(entity);
+            return this.Save();
         }
 
         public int Delete(TEntity entity)
         {
-            throw new NotImplementedException();
+            _table.Remove(entity);
+            return this.Save();
         }
 
         public int Save()
         {
-            throw new NotImplementedException();
+            return _context.SaveChanges();
         }
 
         public IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> predicate = null)
         {
-            throw new NotImplementedException();
+            return predicate == null ? _table : _table.Where(predicate);
         }
     }
 }
