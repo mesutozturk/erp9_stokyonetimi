b429133f0860463935e01b966a50113b6c170ad8
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-09-03 11:13:18 +0300

    connectionstrings

--- Sy.Forms/Form1.Designer.cs
+++ Sy.Forms/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(611, 360);
+            this.ClientSize = new System.Drawing.Size(611, 400);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
