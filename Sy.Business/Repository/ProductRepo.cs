69fc6cb7f8a27899be38ec45e6257869b949cde5
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-09-05 11:54:33 +0300

    list bug fixed

--- Sy.Business/Repository/ProductRepo.cs
+++ Sy.Business/Repository/ProductRepo.cs
@@ -19,7 +19,6 @@ namespace Sy.Business.Repository
                     CriticStock = x.CriticStock,
                     ProductName = x.ProductName,
                 }).ToList();
-            var list = new List<ProductViewModel>();
             foreach (var item in data)
             {
                 var actionList = this.Context.ProductStockActions.Where(x => x.ProductId == item.Id).ToList();
@@ -30,10 +29,10 @@ namespace Sy.Business.Repository
                 var cikanAdet = actionList.Where(x => x.StockActionType == StockActionType.Outgoing)
                     .Sum(x => x.Quantity);
                 item.UnitsInStock = girenAdet - cikanAdet;
-                if (item.UnitsInStock > 0) list.Add(item);
+
             }
 
-            return list;
+            return data;
         }
     }
 }
--- Sy.Forms/CustomerOrderForm.cs
+++ Sy.Forms/CustomerOrderForm.cs
@@ -24,7 +24,7 @@ namespace Sy.Forms
 
         private void ListeyiDoldur(string search = "")
         {
-            lstUrunler.DataSource = ((ProductRepo)_productRepo).GetProductList(search);
+            lstUrunler.DataSource = ((ProductRepo)_productRepo).GetProductList(search).Where(x=>x.UnitsInStock>0).ToList();
             lstUrunler.DisplayMember = "Display";
         }
 
